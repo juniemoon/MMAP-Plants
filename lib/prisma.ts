6c9df0dd04ed2e4a61fc15dda019f56cfd64bc6f
Susanne <mahler-susanne@web.de>
2026-06-16 15:23:09 +0200

    Praktikum_10: 10.1 done + 10.2 done: switch to postgres neon db

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,20 @@
 import { PrismaClient } from "@/generated/prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaBetterSqlite3({ url: "file:prisma/dev.db" });
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
--- prisma.config.ts
+++ prisma.config.ts
@@ -5,11 +5,13 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+
   migrations: {
     path: "prisma/migrations",
     seed: "tsx prisma/seed.ts",
   },
+
   datasource: {
-    url: "file:./prisma/dev.db",
+    url: process.env.DATABASE_URL!,
   },
 });
